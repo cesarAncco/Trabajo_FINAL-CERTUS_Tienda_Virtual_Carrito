3bdb0529428cea5884d6ec59fee69bd67751df8e
cesarAncco <canccor@gmail.com>
2024-05-21 21:57:06 -0500

    Primer Commit: Agregando todos los archivos del catalogo que se desarrollo.

---
+++ products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    id: 1,
+    img: "/img/Laptop_ASUS01.webp",
+    title: "Laptop Asus Vivobook Go 15 R3 7320U 512GB SSD 8 GB RAM 15.6\" Green Grey",
+    store: "Oechsle",
+    price: "S/ 1,799.00"
+  },
+
+  {
+    id: 2,
+    img: "/img/Laptop_LENOVO01.png",
+    title: "Laptop Gamer Lenovo IdeaPad Gaming3 15IAH7 i5-12450H 8GB RAM 512GB SSD RTX3060",
+    store: "Oechsle",
+    price: "S/ 3,529.00"
+  },
+
+  {
+    id: 3,
+    img: "/img/Laptop_ACER01.webp",
+    title: "Laptop Acer A315-24PT Ryzen 5-7520U 16GB RAM 512GB SSD 15,6\"",
+    store: "Oechsle",
+    price: "S/ 2,499.00"
+  },
+
+  {
+    id: 4,
+    img: "/img/Laptop_HP01.webp",
+    title: "Laptop HP 15-fc0002la AMD Ryzen 5 16GB RAM 512GB SSD 15.6\"",
+    store: "Oechsle",
+    price: "S/ 2,499.00"
+  },
+
+  {
+    id: 5,
+    img: "/img/Laptop_APPLE01.webp",
+    title: "Apple Macbook Air MLXW3E/A Chip M2 8GB RAM 256GB 13.6\"",
+    store: "Oechsle",
+    price: "S/ 6,099.00"
+  },
+
+  {
+    id: 6,
+    img: "/img/Laptop_LG01.webp",
+    title: "Laptop LG Gram 17Z90Q-G.AH76 Intel Evo Core i7 12 Núcleos 16GB RAM 512GB SSD 17\"",
+    store: "Oechsle",
+    price: "S/ 6,799.00"
+  },
+
+  {
+    id: 7,
+    img: "/img/Laptop_DELL.webp",
+    title: "Laptop Dell Inspiron I3520 Intel Core i5 8GB RAM 512GB SSD 15.6\"",
+    store: "Oechsle",
+    price: "S/ 1,899.00"
+  },
+  
+]
